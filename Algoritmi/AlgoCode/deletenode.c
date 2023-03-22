d4b890b830ae5156fa52ff0762f64f58f1782cc0
Amerigo Scamardella <aigo@hotmail.it>
2023-03-22 15:48:41 +0100

    reset

---
+++ Algoritmi/AlgoCode/deletenode.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE    10
+void fillArray(int A[],int n);
+
+int main()
+{
+    int Array[SIZE];
+    fillArray(Array,SIZE);
+    puts("Dato un array in MaxHeap")
+    printf("Da")
+    deleteNode(Array,)
+}
